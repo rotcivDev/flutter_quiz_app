bdfe708bb761bca0cdb1fb8c5d212e8be7595e80
Victor Salles <vsallesa@gmail.com>
2023-07-15 16:12:12 -0300

    feat: build the home screen widget

---
+++ lib/screens/home.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/widgets/white_text.dart';
+import 'package:quiz_app/widgets/gradient_container.dart';
+
+const log = print;
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  void startQuiz() {
+    log('Quiz started');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GradientContainer(
+      colors: const [
+        Color.fromARGB(255, 95, 6, 110),
+        Colors.deepPurple,
+      ],
+      column: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/quiz_logo.png',
+              width: 300,
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+            const WhiteText(
+              'Learn Flutter the fun way!',
+              fontSize: 30,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            OutlinedButton(
+              onPressed: startQuiz,
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.white,
+              ),
+              child: const Text(
+                'Start Quiz',
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
