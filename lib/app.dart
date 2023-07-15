10f0013e178c39d0486431c69d700008f8ed72d8
Victor Salles <vsallesa@gmail.com>
2023-07-15 19:41:49 -0300

    refactor: remove app from main file

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/modules/quiz.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Quiz();
+  }
+}
